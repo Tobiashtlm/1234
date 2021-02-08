3666b1ac6d396a3d110181057d2efd360831ea66
tobbe <tobias@openresult.se>
2021-02-08 22:07:24 +0100

    Quiz

--- lule/src/demo/Quiz.scala
+++ lule/src/demo/Quiz.scala
@@ -5,16 +5,56 @@ object Quiz {
 
   def start() = {
 
-    val guess = readLine("Heter Sveriges huvudstad Stockholm? Y/N ")
-    val Y = true
-    val N = false
+    println("Svara genom att ange ett av svarsalternativen (1),(2) eller (3).")
 
+    val guess1 = readLine("""Vem styr Sverige?
+    1. Kungen
+    2. Tobbe
+    3. Jehovas
+    Svar: """)
 
-    if (Y)
-      println("Grattis du svarade rätt! ")
-    else {
-      println("Du svarade fel!")
+    if (guess1 == "2") {
 
+      println("Grattis du svarade rätt på fråga 1! Men den var enkel. ")
+    } else {
+      println("Loser du svarade fel på fråga 1, det börjar inte bra!")
     }
+
+    val guess2 = readLine("""Vilken färg är himmlen?
+    1. Gul
+    2. Silver
+    3. Blå 
+    Svar: """)
+
+    if (guess2 == "3") {
+      println("Grattis du svarade rätt på fråga 2! ")
+    } else {
+      println("Loser du svarade fel på fråga 2!")
+    }
+
+    val guess3 = readLine("""Hur många fötter har en häst?
+    1. 4 fötter
+    2. 2 fötter
+    3. 0 fötter
+    Svar: """)
+
+    if (guess3 == "3") {
+      println("Grattis du svarade rätt på fråga 3! ")
+    } else {
+      println("Loser du svarade fel på fråga 3!")
+    }
+
+    val guess4 = readLine("""Hur många bokstäver finns i ordet Katt? 
+    1. 4st
+    2. 3st
+    3. 30st
+    Svar: """)
+
+    if (guess4 == "1") {
+      println("Grattis du svarade rätt på fråga 4! ")
+    } else {
+      println("Loser du svarade fel på fråga 4!")
+    }
+
   }
 }
--- lule/src/demo/Types.scala
+++ lule/src/demo/Types.scala
@@ -1,41 +1,40 @@
 package demo
 
 object Types {
-    var a: String ="hej" //här deklareras en sträng som heter A
-    val b = "hej då"
-    val i = 42
-    val pi: Double = 3.1415
-    val speech = """ Hejsan 
+  var a: String = "hej" //här deklareras en sträng som heter A
+  val b = "hej då"
+  val i = 42
+  val pi: Double = 3.1415
+  val speech = """ Hejsan 
 detta 
 ska bli 
 4 rader"""
-var firstName = "Jonas"
-var mi = "Bengan"
-var lastName = "Nordsten"
+  var firstName = "Jonas"
+  var mi = "Bengan"
+  var lastName = "Nordsten"
 
-val supernamn = s"$firstName $mi $lastName"
+  val supernamn = s"$firstName $mi $lastName"
 
-    val iIs42 = 42 == i
-    
-    def test () = {
-        val i: Int = 42
+  val iIs42 = 42 == i
 
-        a = "Hello"
-        println(a + "ove rytter")
-        println(b)
-   
-   println(i + pi)
+  def test() = {
+    val i: Int = 42
 
+    a = "Hello"
+    println(a + "ove rytter")
+    println(b)
 
-if (iIs42)
-println ("iIs42 är true")
-else
-    println ("iIs42 är false")
+    println(i + pi)
 
-    println (speech)
-    println (supernamn)
+    if (iIs42)
+      println("iIs42 är true")
+    else
+      println("iIs42 är false")
 
-System.err.println ("Error 404 ")
- 
-    }
+    println(speech)
+    println(supernamn)
+
+    System.err.println("Error 404 ")
+
+  }
 }
